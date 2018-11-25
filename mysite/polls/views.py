53d81b30b9ab9f0a8097072ed6a3c30a34cafb27
TanuAgrawal123 <tanuagrawal1389@gmail.com>
2018-11-25 23:51:58 +0530

    applying some test on view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,13 +4,15 @@ from .models import Question, Choice
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 class IndexView(generic.ListView):
 	template_name='polls/index.html'
 	context_object_name='latest_question_list'
 
 	def get_queryset(self):
-		return Question.objects.order_by('-pub_date')[:5]
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+		 
 
 class DetailView(generic.DetailView):
 	model=Question
