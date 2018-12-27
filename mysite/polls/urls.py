c1125d745814b6caa12d3005bc2b338ee2c1ef4f
TanuAgrawal123 <tanuagrawal1389@gmail.com>
2018-12-27 23:21:45 +0530

    add styling in bootstrap

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from  . import views
 app_name='polls'
 urlpatterns=[
-path('', views.IndexView.as_view(), name='index'),
-path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+path('',views.index, name='index'),
+path('<int:pk>/', views.index, name='detail'),
+path('<int:pk>/results/', views.results, name='results'),
 path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,22 +5,23 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+from django.core.paginator import Paginator
 
-class IndexView(generic.ListView):
-	template_name='polls/index.html'
-	context_object_name='latest_question_list'
 
-	def get_queryset(self):
-		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')
+    paginator = Paginator(latest_question_list, 1)
+    page = request.GET.get('page')
+    questions = paginator.get_page(page)
+    return render(request, 'polls/index.html',{'questions': questions})
 		 
 
-class DetailView(generic.DetailView):
-	model=Question
-	template_name='polls/detail.html'
 
-class ResultsView(generic.DetailView):
-	model=Question
-	template_name='polls/results.html'
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk= question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 		
 def vote(request,question_id):
@@ -28,7 +29,7 @@ def vote(request,question_id):
 	try:
 		selected_choice = question.choice_set.get(pk=request.POST['choice'])
 	except(KeyError, Choice.DoesNotExist):
-		return render(request, 'polls/detail.html', {'question': question, 'error_message': "You didn't select a choice.",})
+		return render(request, 'polls/index.html', {'question': question, 'error_message': "You didn't select a choice.",})
 	else:
 		selected_choice.votes += 1
 		selected_choice.save()
