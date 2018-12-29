2c33d928d9bbb99fcc6fb9425c9da7908fe7ae34
TanuAgrawal123 <tanuagrawal1389@gmail.com>
2018-12-29 20:41:00 +0530

    add a user registeration form and solve issue

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
 path('',views.index, name='index'),
 path('<int:question_id>/results/', views.results, name='results'),
 path('<int:question_id>/vote/', views.vote, name='vote'),
+
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,7 @@
 
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
@@ -9,7 +9,7 @@ from django.core.paginator import Paginator
 
 
 
-def index(request, ):
+def index(request, *args):
     latest_question_list = Question.objects.order_by('-pub_date')
     paginator = Paginator(latest_question_list, 1)
     page = request.GET.get('page')
@@ -29,10 +29,14 @@ def vote(request,question_id):
 	try:
 		selected_choice = question.choice_set.get(pk=request.POST['choice'])
 	except(KeyError, Choice.DoesNotExist):
-		return render(request, 'polls/index.html', {'question': question, 'error_message': "You didn't select a choice.",})
+		return render(request, 'polls/index.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",})
 	else:
 		selected_choice.votes += 1
 		selected_choice.save()
 		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
 
 
