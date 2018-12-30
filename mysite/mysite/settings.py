b2a07bb2938de4b97422b7df73024b46abe10f56
TanuAgrawal123 <tanuagrawal1389@gmail.com>
2018-12-30 22:58:32 +0530

    redirecting issue solved an added some styling using bootstrap

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polls',
-    'user',
+    'crispy_forms'
 
 ]
 
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK='bootstrap4'
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,7 +6,8 @@ from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 from django.core.paginator import Paginator
-
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 
 def index(request, *args):
@@ -38,5 +39,16 @@ def vote(request,question_id):
 		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 
+def register(request):
+	if request.method=='POST':
+		form=UserRegisterForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username=form.cleaned_data.get('username')# dictionary that will give usename
+			messages.success(request, f' Account created for {username}!')
+			return redirect('polls:polls-index')
+	else:
+		form=UserRegisterForm()
+	return render(request, 'polls/register.html', {'form':form})
 
 
